21e2b5a0dcfbd0930563aa7c7f09ebe59eb10387
Iesa <iesa.wazeer7@gmail.com>
2021-07-31 12:23:24 +0100

    Showing the name of people from json file

---
+++ src/components/People.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>{{ name }}</h1>
+</template>
+
+<script>
+export default {
+  name: "People",
+  props: {
+    id: String,
+    age: Number,
+    eyeColor: String,
+    name: String,
+    gender: String,
+    location: Object,
+    preferences: Object,
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,49 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <v-row>
+      <v-col cols="12" v-for="people in data" :key="people._id">
+        <People
+          :name="people.name"
+          :age="people.age"
+          :eyeColor="people.eyeColor"
+          :gender="people.gender"
+          :location="people.location"
+          :preferences="people.location"
+        />
+      </v-col>
+    </v-row>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import People from "@/components/People.vue";
+import json from "@/assets/people.json";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    People,
+  },
+  data() {
+    return {
+      data: json,
+      // selected: {
+      //   age: 0,
+      //   name: '',
+      //   gender: '',
+      //   location: {latitude: 0.0,
+      //   }
+      //   },
+      //   preferences: Object,
+      // }
+    };
+  },
+  // methods: {
+  // },
+
+  mounted() {
+    console.log(this.data);
   },
 };
 </script>
